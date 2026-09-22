cabcd254d8cfd4edb152a044ffc2296f28e7ab23
Krushna <krushnakarwal@gmail.com>
2026-09-22 22:27:46 +0530

    feat: add PredictOutput program

---
+++ src/com/krushna/logic/day_1/PredictOutput.java
@@ -0,0 +1,12 @@
+package com.krushna.logic.day_1;
+
+public class PredictOutput 
+{
+	public static void main(String [] args)
+	{    
+	    int a=5,b=2;
+	    int c = ++a + b++;
+	    System.out.println(c);
+	}
+
+}
