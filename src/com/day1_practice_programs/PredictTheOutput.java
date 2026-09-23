864edeb1829b5a96b0823402e50e33fded94293a
Krushna <krushnakarwal@gmail.com>
2026-09-23 21:48:44 +0530

    feat : Added Predict the Output Program

---
+++ src/com/day1_practice_programs/PredictTheOutput.java
@@ -0,0 +1,12 @@
+package com.day1_practice_programs;
+
+public class PredictTheOutput 
+{
+	public static void main(String[] args) 
+	{
+	    int x=1010;
+	    System.out.println(x%100);
+	    System.out.println(x/10);
+	 }
+}
+
