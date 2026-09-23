86bfd2858c91107e0dd41f5857ec97848545a6ef
Krushna <krushnakarwal@gmail.com>
2026-09-23 21:47:03 +0530

    feat : Added Decrement Operator Program

---
+++ src/com/day1_practice_programs/Decrement_Operator.java
@@ -0,0 +1,11 @@
+package com.day1_practice_programs;
+
+public class Decrement_Operator 
+{
+	public static void main(String [] krushna)
+	{
+	   int x = 3;
+	   System.out.println(x--);
+	   System.out.println(x);
+	 }
+}
