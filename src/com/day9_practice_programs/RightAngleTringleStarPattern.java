acd57a268ce0ac6ea5c40cfd8e8a9be6c95be283
Krushna <krushnakarwal@gmail.com>
2026-09-26 22:24:53 +0530

    feat : Added Right Angle Tringle Pattern Practice Program

---
+++ src/com/day9_practice_programs/RightAngleTringleStarPattern.java
@@ -0,0 +1,26 @@
+package com.day9_practice_programs;
+import java.util.*;
+public class RightAngleTringleStarPattern 
+{
+		public static void main(String [] args)
+		{
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println(" Enter a number :");
+			int n = sc.nextInt();
+			
+			for(int i = 1;i <= n;i++)
+			{
+				for(int j = 1;j <= i;j++)
+				{
+					System.out.print("* ");
+				}
+				
+				System.out.println();
+            }
+			sc.close();
+	}
+
+}
+
+
